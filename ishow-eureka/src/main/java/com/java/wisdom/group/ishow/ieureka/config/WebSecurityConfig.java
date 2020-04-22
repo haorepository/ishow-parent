1299832f2a98927f728d37359eda08216b1977b7
2523796216@qq.com <123456wh@>
2020-04-22 17:45:33 +0800

    update some code

---
+++ ishow-eureka/src/main/java/com/java/wisdom/group/ishow/ieureka/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.java.wisdom.group.ishow.ieureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author ：terry
+ * @date ：Created in 2020/4/22 15:26
+ * @description：TODO
+ * @version: 1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
