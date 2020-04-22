cca1980ed9f9824cd925cd6eec5dda35562accb8
2523796216@qq.com <123456wh@>
2020-04-22 16:05:34 +0800

    new add config files

---
+++ ishow-config-files/src/main/java/com/java/wisdom/group/ishow/iconfigfiles/IshowConfigFilesApplication.java
@@ -0,0 +1,13 @@
+package com.java.wisdom.group.ishow.iconfigfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshowConfigFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IshowConfigFilesApplication.class, args);
+    }
+
+}
