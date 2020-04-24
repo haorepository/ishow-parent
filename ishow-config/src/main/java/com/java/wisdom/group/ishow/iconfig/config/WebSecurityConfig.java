f9a67b02911633069c188c679825e47325447e19
2523796216@qq.com <123456wh@>
2020-04-24 16:39:25 +0800

    update some code

--- ishow-config/src/main/java/com/java/wisdom/group/ishow/iconfig/config/WebSecurityConfig.java
+++ ishow-config/src/main/java/com/java/wisdom/group/ishow/iconfig/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * @author ：terry
@@ -17,6 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 }
