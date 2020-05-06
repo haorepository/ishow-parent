bb25c2c84c6c498c16bd143b6b93ff1ed0dc7453
2523796216@qq.com <123456wh@>
2020-05-06 16:03:40 +0800

    update some code

--- ishow-cache/src/main/java/com/java/wisdom/group/ishow/icache/controller/TestInfoCacheController.java
+++ ishow-cache/src/main/java/com/java/wisdom/group/ishow/icache/controller/TestInfoCacheController.java
@@ -3,6 +3,8 @@ package com.java.wisdom.group.ishow.icache.controller;
 import com.java.wisdom.group.ishow.icache.servcie.TestInfoCacheService;
 import com.java.wisdom.group.ishow.ientity.entity.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description：TODO
  * @version: 1.0
  */
+@RefreshScope
 @RestController
 @RequestMapping(value = "/test-info-cache")
 public class TestInfoCacheController {
@@ -23,9 +26,17 @@ public class TestInfoCacheController {
     @Autowired
     private TestInfoCacheService cacheService;
 
+    @Value("${spring.profiles.active}")
+    private String active;
+
     @RequestMapping(value = "/cache",method = RequestMethod.POST)
     public ResponseEntity<Boolean> cacheTestInfo(@RequestBody TestInfo testInfo){
         boolean flag = cacheService.cacheUser(testInfo);
         return new ResponseEntity<Boolean>(flag,HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/active",method = RequestMethod.GET)
+    public ResponseEntity<String> cacheTestInfo(){
+        return new ResponseEntity<String>(active,HttpStatus.OK);
     }
 }
